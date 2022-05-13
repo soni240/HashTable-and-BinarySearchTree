c92ea174ad0d3a27807cf59c9575e1bb3fb7a4fd
soni240 <s.fegade21@gmail.com>
2022-05-13 19:37:19 +0530

    hash table

--- UC2_frequency_paragraph/MapNode.cs
+++ UC2_frequency_paragraph/MapNode.cs
@@ -1,23 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UC1_frequency_word
+namespace UC2_frequency_paragraph
 {
-    internal partial class MapNode
+    internal class MapNode
     {
+        //Value type Data type KeyValue
+        //Value type Data Type KeyValue
+        public struct KeyValue<K, V>
+        {
+            public K Key { get; set; }
+            public V Value { get; set; }
+        };
 
+      
         public class MapNode<K, V>
         {
             int size;
             public LinkedList<KeyValue<K, V>>[] items;
+
             public MapNode(int size)
             {
                 this.size = size;
                 this.items = new LinkedList<KeyValue<K, V>>[size];
+            }
 
+            public MapNode()
+            {
             }
 
             public void Add(K key, V value)
@@ -92,10 +105,13 @@ namespace UC1_frequency_word
 
                 }
             }
+
+            private string GetDebuggerDisplay()
+            {
+                return ToString();
+            }
         }
     }
+
 }
-
-
-
 
--- UC2_frequency_paragraph/Program.cs
+++ UC2_frequency_paragraph/Program.cs
@@ -1,12 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static UC1_frequency_word.MapNode;
+using static UC2_frequency_paragraph.MapNode;
 
-namespace UC1_frequency_word
+namespace UC2_frequency_paragraph
 {
     class Program
     {
@@ -14,7 +15,8 @@ namespace UC1_frequency_word
         {
 
             Console.WriteLine("***Hashing Problem***");
-            Console.WriteLine("***USE CASE 1-To find frequency of words *** ");
+            Console.WriteLine("***USE CASE 1-To find frequency of words in a sentence*** ");
+            Console.WriteLine("***USE CASE 2-To find frequency of words in a paragraph** ");
             Console.WriteLine("Enter the option");
             int num = Convert.ToInt32(Console.ReadLine());
             MapNode<string, int> myMapNode = new MapNode<string, int>(6);
@@ -42,12 +44,43 @@ namespace UC1_frequency_word
                         myMapNode.Display(i);
                     }
                     break;
+                case 2:
+
+                    MapNode<string, int> myMap = new MapNode<string, int>(10);
+                    string[] Paragraph;
+                    string input = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+                    Paragraph = input.Split(' ');
+                    //Given string input
+
+                    int counts = 1;
+                    foreach (string i in Paragraph)
+                    {
+                        counts = myMap.CheckHash(i);
+                        if (counts > 1)
+                        {
+                            myMap.Add(i, counts);
+                        }
+                        else
+                        {
+                            myMap.Add(i, 1);
+                        }
+                    }
+                    Console.WriteLine("\n---------Frequency of words in paragraph---------\n");
+                    IEnumerable<string> distinct = Paragraph.Distinct<string>();
+                    foreach (var i in distinct)
+                    {
+                        myMap.Display(i);
+                    }
+
+                    break;
+
                 default:
                     Console.WriteLine("Enter the valid option!!!");
                     break;
+
             }
+
             Console.ReadLine();
         }
     }
 }
-
